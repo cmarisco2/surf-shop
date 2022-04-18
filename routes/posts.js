3768ecf6a42613aa57c0500172e3fabf6ea293a9
Christopher Marisco <christopher.marisco@gmail.com>
2022-04-18 10:57:31 -0600

    add boilerplate routes to the user post routes

--- routes/posts.js
+++ routes/posts.js
@@ -3,11 +3,39 @@ const router = express.Router();
 
 /* GET posts index /posts. */
 router.get('/', (req, res, next) => {
-    res.send('/posts');
+    res.send('index posts');
 });
 
-/* GET new post form */
+/* GET new post form /posts/new */
 router.get('/new', (req, res, next) => {
-    res.send('/posts/new');
+    res.send('retrieve post form');
+});
+
+/* POST /posts */
+router.post('/', (req, res, next) => {
+    res.send('create post');
+});
+
+/* GET show post form /posts/:id */
+router.get('/:id', (req, res, next) => {
+    const { id } = req.body;
+    res.send(`Show post: /posts/${id}`);
+});
+
+/* GET post edit form /posts/:id/edit */
+router.get('/:id/edit', (req, res, next) => {
+    const { id } = req.body;
+    res.send(`edit post: /posts/${id}/edit`);
+});
+/* PUT post form /posts/:id/ */
+router.put('/:id', (req, res, next) => {
+    const { id } = req.body;
+    res.send(`Update Post: ${id}`);
+});
+
+/* DELETE show post form /posts/:id/ */
+router.delete('/:id', (req, res, next) => {
+    const { id } = req.body;
+    res.send(`delete post: ${id}`);
 });
 module.exports = router;
